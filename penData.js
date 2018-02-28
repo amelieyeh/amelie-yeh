86e452e21bedd7aa58fa20f11d828b45465e1cea
Amelie Yeh <amelieyeh@gmail.com>
2018-02-28 22:28:10 +0800

    Updates

--- penData.js
+++ penData.js
@@ -1,12 +1,12 @@
 module.exports = {
   pen1: {
     title: 'Pure CSS - Rosseta & Philea',
-    img: '/pens/pen1.png',
+    img: 'pen1.png',
     link: 'https://codepen.io/amelieyeh/pen/GymEEV'
   },
   pen2: {
     title: 'Pure CSS - Crystal loader w/ animation',
-    img: '/pens/pen2.png',
+    img: 'pen2.png',
     link: 'https://codepen.io/amelieyeh/pen/RxpjEW/'
   }
 }
