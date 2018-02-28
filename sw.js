6f94715b6537343c98aa4c857423c043f4cb9e1a
Amelie Yeh <amelieyeh@gmail.com>
2018-02-28 21:03:56 +0800

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/amelie-yeh/app-99bc6e4c6b33161389c8.js","c44385c7d95d4eacf1b79f9181016ca6"],["/amelie-yeh/commons-2ac0f93e031bc057f82c.js","b2e09fe1487f83f06dab0cd8d65bf785"],["/amelie-yeh/commons-56d86438a9635fd326bc.js","379ca611bafda96a451ac12ad492b951"],["/amelie-yeh/commons-6662b0984c6652304a0d.js","6e805ccd5aceb73598d22eca27c67483"],["/amelie-yeh/index.html","ca4cfe68f712368aed120c8b4fd146d9"],["/amelie-yeh/manifest.json","ce893c31ff483d60b0f820ee4137081f"],["/amelie-yeh/offline-plugin-app-shell-fallback/index.html","e8d947984b9906a520af4ddcbd5a020f"]];
+var precacheConfig = [["/amelie-yeh/app-99bc6e4c6b33161389c8.js","c44385c7d95d4eacf1b79f9181016ca6"],["/amelie-yeh/app-e7e21ddea52632c16546.js","3a4b0b4a267037226a299b2bf7575457"],["/amelie-yeh/commons-2ac0f93e031bc057f82c.js","b2e09fe1487f83f06dab0cd8d65bf785"],["/amelie-yeh/commons-56d86438a9635fd326bc.js","379ca611bafda96a451ac12ad492b951"],["/amelie-yeh/commons-6662b0984c6652304a0d.js","6e805ccd5aceb73598d22eca27c67483"],["/amelie-yeh/commons-8d67b54d422472f26eed.js","1f62ce376dd092e1a688ae4ce40dcd82"],["/amelie-yeh/index.html","4abb8449328321da1a9a8f20f0421d17"],["/amelie-yeh/manifest.json","ce893c31ff483d60b0f820ee4137081f"],["/amelie-yeh/offline-plugin-app-shell-fallback/index.html","527c11d8f88c70843fd02ec9c4d65ada"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
