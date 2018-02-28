785df7961b7b8f5a3fbf77f68d8b1c596b069634
Amelie Yeh <amelieyeh@gmail.com>
2018-02-28 22:16:42 +0800

    Updates

--- penData.js
+++ penData.js
@@ -1,12 +1,12 @@
 module.exports = {
   pen1: {
     title: 'Pure CSS - Rosseta & Philea',
-    img: 'amelie-yeh/pens/pen1.png',
+    img: '/pens/pen1.png',
     link: 'https://codepen.io/amelieyeh/pen/GymEEV'
   },
   pen2: {
     title: 'Pure CSS - Crystal loader w/ animation',
-    img: 'amelie-yeh/pens/pen2.png',
+    img: '/pens/pen2.png',
     link: 'https://codepen.io/amelieyeh/pen/RxpjEW/'
   }
 }
--- postData.js
+++ postData.js
@@ -1,12 +1,12 @@
 module.exports = {
   post1: {
     title: 'Flexbox VS CSS Grid 1 — Flexbox',
-    img: 'amelie-yeh/posts/post1.png',
+    img: '/posts/post1.png',
     link: 'https://medium.com/binarycake/flexbox-vs-css-grid-1-flexbox-eede091377f5'
   },
   post2: {
     title: 'Flexbox VS CSS Grid 2 — CSS Grid',
-    img: 'amelie-yeh/posts/post2.png',
+    img: '/posts/post2.png',
     link: 'https://medium.com/binarycake/flexbox-vs-css-grid-2-css-grid-e063d5e305c0'
   }
 }
